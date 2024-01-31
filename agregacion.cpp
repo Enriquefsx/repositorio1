db8ad12f530f9f54f539c5c7acead00b4d3ef873
Enrique <jorgitofsx@gmail.com>
2024-01-30 23:25:49 -0500

    primer commit

---
+++ agregacion.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string name;
+    cout<<"proyect"<<endl<<endl;
+    cin>>name;
+    cout<<endl<<endl<<name;
+    return 0;                   
+}
